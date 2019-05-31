c1fcb0b3b4e7dfe7f33b092ba040651f0f4a7c6c
mkorzeniowska <51078508+mkorzeniowska@users.noreply.github.com>
2019-05-31 10:59:51 +0200

    Add files via upload

---
+++ Selenium WebDriver + C#/ExamXPathTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace CourseTest
+{
+    [TestClass]
+    public class ExamXPathTest
+    {
+        public IWebDriver Driver;
+
+        [TestInitialize]
+        public void CreateDriver()
+        {
+            Driver = GetChromeDriver();
+        }
+        public IWebDriver GetChromeDriver()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outPutDirectory);
+        }
+        [TestMethod]
+        public void SeleniumElementLocations()
+        {
+            Driver.Navigate().GoToUrl("https://www.ultimateqa.com/simple-html-elements-for-automation/");
+            ScrollToElementUsingJavaScript(By.XPath("//*[@type='radio']/../.."));
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Driver.FindElement(By.XPath("//*[@type='radio'][@value='female']")).Click();
+            Driver.FindElement(By.XPath("//*[@name='vehicle'][@value='Bike']")).Click();
+            Driver.FindElement(By.XPath("//*[@value='audi']")).Click();
+            var link = Driver.FindElement(By.XPath("//*[contains(text(),'Tab 2')]"));
+            link.Click();
+            string.Equals("Tab 2 content",link.Text);
+            HighLightElementUsingJavaScript(By.XPath("//*[contains(text(),'$50,000+')]"));
+        }
+        
+
+        [TestMethod]
+        public void ScrollToElementUsingJavaScript(By locationStrategy)
+        {
+            var element = Driver.FindElement(locationStrategy);
+            IJavaScriptExecutor javaScriptExecutor = Driver as IJavaScriptExecutor;
+            javaScriptExecutor.ExecuteScript("arguments[0].scrollIntoView();", element);
+        }
+        [TestMethod]
+        public void HighLightElementUsingJavaScript(By locationStrategy, int duration =2)
+        {
+            var element = Driver.FindElement(locationStrategy);
+            var originalStyle = element.GetAttribute("style");
+            IJavaScriptExecutor javaScriptExecutor = Driver as IJavaScriptExecutor;
+            javaScriptExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1], arguments[2])",
+                element, "style", "border: 7px solid yellow; border-style: dashed");
+
+            if (duration <= 0) return;
+            Thread.Sleep(TimeSpan.FromSeconds(duration));
+            javaScriptExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1], arguments[2])", element, "style", originalStyle);
+
+        }
+
+
+    }
+}
---
+++ Selenium WebDriver + C#/WebElementsTest.cs
@@ -0,0 +1,62 @@
+﻿using AutomationResources;
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace CourseTest
+{
+    [TestClass]
+    [TestCategory("Locating web elements")]
+    public class WebElementsTest
+    {
+        public IWebDriver Driver { get; set; }
+
+        [TestInitialize]
+        public void RunBeforeEveryTests()
+        {
+            Driver = new WebDriverFactory().Create(BrowserType.Chrome);
+        }
+        [TestMethod]
+        public void DifferentSeleniumLocations()
+        {
+            Driver.Navigate().GoToUrl("https://www.ultimateqa.com/simple-html-elements-for-automation/");
+            HighligtElementUsingJavaScript(By.ClassName("buttonClass"));
+            HighligtElementUsingJavaScript(By.Id("idExample"));
+            HighligtElementUsingJavaScript(By.LinkText("Click me using this link text!"));
+            HighligtElementUsingJavaScript(By.Name("button1"));
+            HighligtElementUsingJavaScript(By.PartialLinkText("link text"));
+            HighligtElementUsingJavaScript(By.TagName("div"));
+            HighligtElementUsingJavaScript(By.CssSelector("#idExample"));
+            HighligtElementUsingJavaScript(By.CssSelector(".buttonClass"));
+            HighligtElementUsingJavaScript(By.XPath("//*[@id='idExample']"));
+            HighligtElementUsingJavaScript(By.XPath("//*[@class='buttonClass']"));
+        }
+        [TestMethod]
+        public void HighligtElementUsingJavaScript(By locationStrategy, int duration =2)
+        {
+            var element = Driver.FindElement(locationStrategy);
+            var originalStyle = element.GetAttribute("style");
+            IJavaScriptExecutor javaScriptExecutor = Driver as IJavaScriptExecutor;
+            javaScriptExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1], arguments[2])",
+                 element,
+                 "style",
+                 "border: 7px solid yellow; border-style: dashed");
+
+            if (duration <= 0) return;
+            Thread.Sleep(TimeSpan.FromSeconds(duration));
+            javaScriptExecutor.ExecuteScript("arguments[0].setAttribute(arguments[1], arguments[2])",
+                element,
+                "style",
+                originalStyle);
+        }
+        [TestCleanup]
+        public void CleanupAfterEveryTest()
+        {
+            Driver.Quit();
+        }
+    }
+}
